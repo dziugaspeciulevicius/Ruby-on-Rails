cb886fae6e8a33830475974854bdfbe811e8e7af
dziugasp <dziugaspeciulevicius@gmail.com>
2020-10-22 16:17:12 +0300

    add task4 rectangle testing /w RSpec

--- README.md
+++ README.md
@@ -25,3 +25,29 @@ For all programs in Task 2 make the following changes:
 2. Install Gem: Rubocop
 
 3. Fix all Rubocop errors.
+
+
+### TASK4
+***Task description:***
+
+Select any Test Framework of your choice (for example Ruby Unit Test).
+
+Using Test Framework, write tests for Task 3 programs (1 program per person).
+
+In this task I used RSpec to test my application. RSpec is a testing tool for for Ruby applications. It is considered a 
+behavior driven development framework. The tests look at a ruby object or class, 
+and describes what that object should be doing. It is intended to be very intuitive, like the rest of ruby. 
+It's used in a lot of ruby and rails enterprise level applications.
+
+[You can find a link for documentation here.](http://rspec.info/documentation/) 
+
+To get started with RSpec first we need to install rspec:
+```
+gem install rspec
+```
+
+To set it up in a project, run this command below:
+```
+rspec --init
+```
+
---
+++ TASK4/rectangle/rectangle.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+# importing rectangleClass
+require_relative 'rectangle_class'
+
+# gets.chomp.to_i basically converts user input into float
+puts 'Please enter the length of a rectangle: '
+length = gets.chomp.to_f
+puts 'Please enter the width of a rectangle: '
+width = gets.chomp.to_f
+puts 'Please enter coordinate x: '
+x_coordinate = gets.chomp.to_i
+puts 'Please enter coordinate y: '
+y_coordinate = gets.chomp.to_i
+
+rectangle = RectangleClass.new(length, width, x_coordinate, y_coordinate)
+
+# taking user input and then putting them into a variable
+calculated_perimeter = rectangle.get_perimeter(width, length)
+calculated_area = rectangle.get_area(width, length)
+calculated_diagonal = rectangle.get_diagonal_length(width, length)
+calculated_coordinates =
+  rectangle.get_coordinate_x(x_coordinate, length) + rectangle.get_coordinate_y(y_coordinate, width)
+
+# outputting information to a user
+puts "The perimeter of a rectangle is: #{calculated_perimeter}"
+puts "The area of a rectangle is: #{calculated_area}"
+puts "The diagonal of a rectangle is: #{calculated_diagonal}"
+puts "The rectangles diagonals intersection coordinates are: #{calculated_coordinates}"
